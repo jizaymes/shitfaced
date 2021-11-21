3107b01bb418d7eb0020003ba785c811839acaf6
James Cornman <jizaymes@gmail.com>
2021-11-21 11:16:54 -0600

    Add jinja2 templating and refactored image scaling a lot. Go math

--- app.py
+++ app.py
@@ -1,31 +1,24 @@
 import shitfaced
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.responses import HTMLResponse, StreamingResponse
+from fastapi.templating import Jinja2Templates
+
 from io import BytesIO
 
 # You can change this to any folder on your system
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def allowed_file(file):
+    return '.' in file.filename and file.filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.get('/')
-async def root_get():
-    content = """
-    <body>
-    <form action="/" enctype="multipart/form-data" method="post">
-    <input name="file" type="file">
-    <input type="submit">
-    </form>
-    </body>
-    """
-
-    return HTMLResponse(content)
+@app.get('/', response_class=HTMLResponse)
+async def root_get(request: Request):
+    return templates.TemplateResponse("index.html", {'request': request})
 
 
 @app.post('/')
@@ -34,10 +27,17 @@ async def upload_file(file: UploadFile = File(...)):
         with BytesIO(image) as file_like:
             yield from file_like
 
-    if file and allowed_file(file.filename):
+    if file and allowed_file(file):
         # The image file seems valid! Detect faces and return the result.
         content = await file.read()
-        image = shitfaced.process_image(content, file.filename)
+        image = shitfaced.process_image(content, shitfaced.DEBUG)
+
+        if image is False:
+            content = """
+            Invalid file format
+            """
+
+            return HTMLResponse(content)
 
         return StreamingResponse(iterimage(image.getvalue()), media_type=file.content_type)
     else:
--- shitfaced.py
+++ shitfaced.py
@@ -1,13 +1,14 @@
 from PIL import Image, ImageDraw
 from pathlib import Path
 import face_recognition
+from io import BytesIO
 from rich import print
-import io
 
 OVERLAY_IMAGE = Path('./poop.png')
 IMAGE_MODE = "RGBA"
 OUTPUT_FORMAT = 'PNG'
-RESIZE_SCALE = .05
+RESIZE_SCALE = .1
+
 # DEBUG = True
 DEBUG = False
 
@@ -17,60 +18,70 @@ def debugLog(msg):
 
 
 def get_faces_from_image(infile):
-    image = face_recognition.load_image_file(io.BytesIO(infile))
+    image = face_recognition.load_image_file(BytesIO(infile))
     face_locations = face_recognition.face_locations(image)
     face_encodings = face_recognition.face_encodings(image, face_locations)
     return face_locations, face_encodings
 
 
-def process_image(infile, filename: str, drawRectangle=False):
-    image = Image.open(io.BytesIO(infile))
+def apply_scaling(top, right, bottom, left, scale: float):
+    # Dimensions of the face that we found
+    height = round(right - left)
+    width = round(bottom - top)
+    debugLog(f"Old {height} {width}")
+
+    aspect_ratio = width / height
+
+    new_height = round(height * (1 + scale))
+    new_width = round(width * (1 + scale))
+    debugLog(f"New {new_height} {new_width}")
+
+    # Apply scale to the bounding coordinates so our overlay can be bigger..if scale says so
+    tt = round(top - (new_height - height))
+    rr = round((left + new_width) * aspect_ratio)
+    bb = round((top + new_height) * aspect_ratio)
+    ll = round(left - (new_width - width))
+
+    return tt, rr, bb, ll
+
+
+def process_image(infile, drawRectangle=False):
+
+    # Convert the incoming image to a Pillow image in memory, and conver to the right mode
+    infile_image = Image.open(BytesIO(infile)).convert(IMAGE_MODE)
+
+    h, w = infile_image.size
+    if h < 150 and w < 150:
+        return False
+
+    # Load the overlay image and convert it to the right mode
+    overlay_image = Image.open(OVERLAY_IMAGE).convert(IMAGE_MODE)
 
     face_locations, face_encodings = get_faces_from_image(infile)
-
-    infile_image = image.convert(IMAGE_MODE)
-    overlay_image = Image.open(OVERLAY_IMAGE).convert(IMAGE_MODE)
-    output_image = infile_image.copy()
 
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
         debugLog("-- New Face {{")
         debugLog(f"Top {top}, Right {right}, Bottom {bottom}, Left {left}")
 
-        # Dimensions of the face that we found
-        height = round(right - left)
-        width = round(bottom - top)
+        tt, rr, bb, ll = apply_scaling(top, right, bottom, left, RESIZE_SCALE)
 
-        debugLog(f"  Face Height, Weight ({height}, {width})")
+        debugLog("  New face params")
+        debugLog(f"Top {tt}, Right {rr}, Bottom {bb}, Left {ll}")
 
-        start_left = left
-        start_top = top
-        debugLog(f"  Starting X, Y ({start_left}, {start_top})")
-
-        height = int((right - left) * (1 + RESIZE_SCALE))
-        width = int((bottom - top) * (1 + RESIZE_SCALE))
-
-        debugLog(f"  Updated Face Height, Weight ({height}, {width})")
-
-        start_left = round(left - round(left * RESIZE_SCALE))
-        start_top = round(top - round(top * RESIZE_SCALE))
-
-        debugLog(f"  Updated X, Y ({start_left}, {start_top})")
-
-        draw = ImageDraw.Draw(output_image) if drawRectangle else False
-
-        ll = round(left * (1 - RESIZE_SCALE))
-        tt = round(top * (1 - RESIZE_SCALE))
-        rr = round(right * (1 + RESIZE_SCALE))
-        bb = round(bottom * (1 + RESIZE_SCALE))
-        debugLog(f"  New face params {ll}, {tt}, {rr}, {bb}")
-
+        # resize our overlay image to match this size
         tmp_overlay = overlay_image.resize((rr - ll, bb - tt))
 
-        draw.rectangle(((ll, tt), (rr, bb)), outline=(0, 255, 0)) if drawRectangle else False
-        output_image.paste(tmp_overlay, (ll, tt), tmp_overlay)
+        # If we're in debug mode/drawRectangle=True
+        if drawRectangle:
+            draw = ImageDraw.Draw(infile_image)
+            draw.rectangle(((ll, tt), (rr, bb)), outline=(0, 255, 0))
+
+        # Apply the overlay
+        infile_image.paste(tmp_overlay, (ll, tt), tmp_overlay)
 
         debugLog("}} End Face")
 
-    img_byte_arr = io.BytesIO()
-    output_image.save(img_byte_arr, format=OUTPUT_FORMAT)
+    img_byte_arr = BytesIO()
+    infile_image.save(img_byte_arr, format=OUTPUT_FORMAT)
+
     return img_byte_arr
